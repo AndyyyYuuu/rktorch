52e9264cdc6d26c933ee092d31e6baf4e7361808
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2026-02-05 19:17:04 -0500

    Contracts, documentation, refactoring

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
     <i>Gradient descent optimization in a purely-functional teaching language </i><br><br>
 </div>
 
-This is a demo of a toy neural network, specifically an MLP with `tanh` activation. Performing backprop in Racket turned out to be a lot more difficult than I expected. I hope the resulting implementation is correct, and feel free to open an issue if there are any mistakes. 
+This is a demo of a toy neural network, specifically an MLP with `tanh` activation. Performing backprop in Racket using a limited set of functions turned out to be a lot more difficult than I expected. I hope the resulting implementation is correct, and feel free to open an issue if there are any mistakes. 
 
 The training script was built entirely in the purely-functional "Intermediate Student with <tt>lambda</tt>" teaching language of Racket. I wrote it in preparation for my CS 135 final at UWaterloo. Consequently, it's constrained strictly to the use of "allowed constructs" from CS 135. <sup>You happy now, Prof. Roh?</sup>
 
@@ -17,7 +17,7 @@ This script can be run with 4 easy steps:
 3. Open `rktorch.rkt` with DrRacket.
 4. Hit `Run ▶`.
 
-The program will output, among other things, the train and test loss lists. Keep in mind that since I use `cons` to push to the beginning of my lists, the losses are appended in reverse order. If you see two lists of increasing numbers, it actually means that the loss is decreasing and that the optimization is working. 
+The program will output, among other things, the train and test loss lists. Keep in mind that since I use `cons` to push to the beginning of my lists, the losses are appended in reverse order. Lists of increasing numbers indicate that the loss is decreasing and that the optimization is working. 
 
 ## Overview
 I will provide a short overview of how this implementation works. 
@@ -28,17 +28,14 @@ The algorithm is centered around using `Tensor` objects to build a computation t
 A `Tensor` object is a `list` with 6 elements: 
 1. The **value** of the Tensor, an `NdArr`
 2. The **gradient** of the Tensor with respect to the loss, another `NdArr`
-3. A `Sym` that tags the Tensor for ease of tracing
+3. A `Sym` that tags the Tensor for ease of searching, debugging. This might be one of the following: 
     - `'matmul`, `'tanh`, `'add`, `'mse`: the tensor is not a leaf node and was produced through the operation (matrix multiplication, hyperbolic tangent, elementwise addition, mean-squared error respectively)
     - `'param`: a leaf node parameter in the neural network that can be optimized by `optim/step`
-    - `'X`: an input Tensor from the dataset which is replaced before performing each forward pass
-    - `'Y`: a ground-truth output substituted from the dataset for each loss computation
-    - `'Y-pred`: the output of the model
-4. A list of all tensors that were involved in the previous operation to compute this tensor. This may include one (for tanh), two (for addition), or no (for leaf nodes) tensors. 
+    - `'X` `'Y`: input and ground-truth output tensors, substituted from the training set before each forward pass
+4. A list of all tensors that were involved in the previous operation to compute this tensor, or empty if the tensor is a leaf node. 
     - Recursively, every tensor includes all earlier tensors in its computation graph.
-    - In that sense, the root node is sufficient representation of the model as a whole.
+    - In that sense, the root node encompasses the entire model. 
 5. A **forward pass function** takes the tensor itself as input and outputs an updated version of itself. It first triggers forward pass functions in child nodes, then performs its operation on its own child nodes to obtain the updated value. 
-6. A **backward pass function** that takes the tensor itself as input and updates the gradients of its own child nodes. Also triggers backward pass functions of the child nodes so that the entire graph can be updated recursively. 
+6. A **backward pass function** that takes the tensor itself as input and updates the gradients of its own child nodes. Also triggers backward pass functions of the child nodes. 
 
-The rest of the functionality mostly builds upon the use of this framework. 
-
+The rest of the functionality mostly builds upon the use of this framework. The forward and backward passes are triggered recursively, depth-first, throughout the entire network by running the pass functions in the root node. 
--- rktorch.rkt
+++ rktorch.rkt
@@ -7,17 +7,22 @@
 ;; A few basic operations
 ;; ~~~~~~~~~~
 
+;; tanh: Num -> Num
 (define (tanh x)
   (/ (- 1 (exp (* -2 x))) (+ 1 (exp (* -2 x)))))
 
+;; tanh: Num -> Num
 (define (tanh-deriv x)
   (- 1 (expt (tanh x) 2)))
 
+;; list/nth: Nat (listof X) -> X
+;; Requires: the index is valid
 (define (list/nth n lst)
   (cond
     [(zero? n) (first lst)]
     [else (list/nth (sub1 n) (rest lst))]))
 
+;; list/mean: (listof Num) -> Num
 (define (list/mean lst)
   (/ (foldr + 0 lst) (length lst)))
 
@@ -26,14 +31,17 @@
 
 (define random/uniform/precision 10000)
 
+;; random/uniform: Num Num -> Num
 (define (random/uniform a b)
   (+ a (* (- b a) (/ (+ (random random/uniform/precision) 1) random/uniform/precision))))
 
+;; random/gaussian: Num Num -> Num
 (define (random/gaussian mean sd)
   (+ mean (* sd (sqrt (* -2 (log (random/uniform 0 1)))) (cos (* 2 pi (random/uniform 0 1))))))
 
 (define noise/sd 0.03)
 
+;; random/add-noise: Num -> Num
 (define (random/add-noise n)
   (+ n (random/gaussian 0 noise/sd)))
 
@@ -101,7 +109,8 @@
                       (lambda (i) (ndarr/build (rest shape)
                                                (lambda (idx) (fn (cons i idx))))))]))
 
-
+;; ndarr/transpose: NdArr -> NdArr
+;; Requires: m is a 2d matrix
 (define (ndarr/transpose m)
   (local
     [(define s (ndarr/shape m))]
@@ -110,22 +119,26 @@
 ;(check-expect (ndarr/transpose '((1 2) (2 3) (3 4))) '((1 2 3) (2 3 4)))
 
 ;; Takes the dot product of two NdArrs
-;; ndarr/dot: NdArr NdArr -> Ndarr
+;; ndarr/dot: NdArr NdArr -> NdArr
 ;; Requires: (first (shape v)) = (first (shape w))
 (define (ndarr/dot v w)
   (cond
     [(empty? (rest v)) (ndarr/elementwise * (first v) (first w))] ; Not pretty but I can't broadcast + 0
     [else (ndarr/elementwise + (ndarr/elementwise * (first v) (first w)) (ndarr/dot (rest v) (rest w)))]))
 
+;; ndarr/mse: NdArr NdArr -> NdArr
+;; Requires: v1 and v2 are vectors of equal shape
 (define (ndarr/mse v1 v2)
   (ndarr/elementwise / (foldr (lambda (x y) (ndarr/elementwise + x y))
             0
             (ndarr/elementwise (lambda (a b) (ndarr/map sqr (ndarr/elementwise - a b))) v1 v2)) (length v1)))
 
+;; ndarr/vm-mul: NdArr NdArr -> NdArr
 (define (ndarr/vm-mul v m)
   (cond [(= (length v) (length (first m)))
          (map (lambda (r) (ndarr/dot v r)) m)]))
 
+;; ndarr/outer-prod: NdArr NdArr -> NdArr
 (define (ndarr/outer-prod vi vj)
   (map (lambda (r) (map (lambda (c) (ndarr/elementwise * c r)) vj)) vi))
 
@@ -171,17 +184,25 @@
 ;; Tensor functions
 ;; ~~~~~~~~~~
 
-;; An Op is (anyof 'matmul 'tanh 'relu 'add 'none)
+;; An Op is (anyof 'matmul 'tanh 'relu 'add)
+;; A Tag is (anyof Op 'param 'X 'Y 'Y-pred)
 
-;; A Tensor is a (listof NdArr NdArr Op (listof Tensor) Tensor ())
+;; A Tensor is a (listof NdArr NdArr Tag (listof Tensor) (Tensor -> Tensor) (Tensor -> (listof Tensor)))
 
-(define (mk-tensor ndarr grad prev-op presyns forward backward)
-  (cond [(and (symbol? prev-op) (list? presyns) (or (empty? presyns) (and (list? (first presyns)) (empty? (rest presyns))) (and (list? (first presyns)) (list? (second presyns)))))
-         (list ndarr grad prev-op presyns forward backward)]))
+;; tensor/mk-tensor: (listof NdArr NdArr Tag (listof Tensor) (Tensor -> Tensor) (Tensor -> (listof Tensor))) -> Tensor
+(define (mk-tensor ndarr grad tag presyns forward backward)
+  (cond [(and (symbol? tag) ; type checking condition
+              (list? presyns)
+              (or (empty? presyns)
+                  (and (list? (first presyns)) (empty? (rest presyns)))
+                  (and (list? (first presyns)) (list? (second presyns)))))
+         (list ndarr grad tag presyns forward backward)]))
 
+;; tensor/copy: Tensor -> Tensor
 (define (tensor/copy t)
-  (mk-tensor (tensor/ndarr t) (tensor/grad t) (tensor/prev-op t) (tensor/presyns t) (tensor/forward t) (tensor/backward t)))
+  (mk-tensor (tensor/ndarr t) (tensor/grad t) (tensor/tag t) (tensor/presyns t) (tensor/forward t) (tensor/backward t)))
 
+;; Tensor Getters
 
 (define (tensor/ndarr tensor)
   (first tensor))
@@ -189,7 +210,7 @@
 (define (tensor/grad tensor)
   (second tensor))
 
-(define (tensor/prev-op tensor)
+(define (tensor/tag tensor)
   (third tensor))
 
 (define (tensor/presyns tensor)
@@ -201,50 +222,60 @@
 (define (tensor/backward tensor)
   (sixth tensor))
 
-(define (tensor/new-grad t grad)
-  (mk-tensor (tensor/ndarr t) grad (tensor/prev-op t) (tensor/presyns t) (tensor/forward t) (tensor/backward t)))
+;; Tensor Setters
 
-(define (tensor/new-presyns t presyns)
-  (mk-tensor (tensor/ndarr t) (tensor/grad t) (tensor/prev-op t) presyns (tensor/forward t) (tensor/backward t)))
+;; tensor/set-grad: Tensor NdArr -> Tensor
+(define (tensor/set-grad t grad)
+  (mk-tensor (tensor/ndarr t) grad (tensor/tag t) (tensor/presyns t) (tensor/forward t) (tensor/backward t)))
 
-;; Leaf node construction
-(define (tensor/new-param ndarr)
-  (mk-tensor (ndarr/build (ndarr/shape ndarr) (lambda (_) (random/gaussian 0 0.2))) (ndarr/fill (ndarr/shape ndarr) 0) 'param '() (lambda (self) (tensor/copy self)) (lambda (x) x)))
+;; tensor/set-presyns: Tensor (listof Tensor) -> Tensor
+(define (tensor/set-presyns t presyns)
+  (mk-tensor (tensor/ndarr t) (tensor/grad t) (tensor/tag t) presyns (tensor/forward t) (tensor/backward t)))
 
+;; Create a parameter
+;; tensor/set-grad: Shape -> Tensor
+(define (tensor/new-param shape)
+  (mk-tensor (ndarr/build shape (lambda (_) (random/gaussian 0 0.2))) (ndarr/fill shape 0) 'param '() (lambda (self) (tensor/copy self)) (lambda (x) x)))
+
+;; Create an input node
+;; tensor/new-input: NdArr -> Tensor
 (define (tensor/new-input ndarr)
   (mk-tensor ndarr (ndarr/fill (ndarr/shape ndarr) 0) 'X '() (lambda (self) (tensor/copy self)) (lambda (x) x)))
 
+;; Create an expected output node
+;; tensor/new-targ: NdArr -> Tensor
 (define (tensor/new-targ ndarr)
   (mk-tensor ndarr (ndarr/fill (ndarr/shape ndarr) 0) 'Y '() (lambda (self) (tensor/copy self)) (lambda (x) x)))
 
 
 (define (tensor/param? t)
-  (symbol=? 'param (tensor/prev-op t)))
+  (symbol=? 'param (tensor/tag t)))
 
 (define (tensor/input? t)
-  (symbol=? 'X (tensor/prev-op t)))
+  (symbol=? 'X (tensor/tag t)))
 
 (define (tensor/targ? t)
-  (symbol=? 'Y (tensor/prev-op t)))
+  (symbol=? 'Y (tensor/tag t)))
 
 (define (tensor/output? t)
-  (symbol=? 'Y-pred (tensor/prev-op t)))
+  (symbol=? 'Y-pred (tensor/tag t)))
 
 (define (tensor/leaf? t)
-  (or (symbol=? 'param (tensor/prev-op t))
-      (symbol=? 'X (tensor/prev-op t))
-      (symbol=? 'Y (tensor/prev-op t))))
+  (or (symbol=? 'param (tensor/tag t))
+      (symbol=? 'X (tensor/tag t))
+      (symbol=? 'Y (tensor/tag t))))
 
-(define (tensor/new-ndarr t value)
-  (mk-tensor value (tensor/grad t) (tensor/prev-op t) (tensor/presyns t) (tensor/forward t) (tensor/backward t)))
+(define (tensor/set-ndarr t value)
+  (mk-tensor value (tensor/grad t) (tensor/tag t) (tensor/presyns t) (tensor/forward t) (tensor/backward t)))
 
-(define (tensor/flag t value)
+(define (tensor/set-tag t value)
   (mk-tensor (tensor/ndarr t) (tensor/grad t) value (tensor/presyns t) (tensor/forward t) (tensor/backward t)))
 
 
 ;; Differentiable Tensor Functions
 ;; ~~~~~~~~~~
 
+;; tensor/add: Tensor Tensor -> Tensor
 (define (tensor/add t1 t2)
   (mk-tensor (ndarr/elementwise + (tensor/ndarr t1) (tensor/ndarr t2))
              (ndarr/fill (ndarr/shape (tensor/ndarr t1)) 0)
@@ -267,9 +298,10 @@
                   (local
                     [(define t1 (first (tensor/presyns out-t)))
                      (define t2 (second (tensor/presyns out-t)))]
-                    (tensor/new-presyns out-t (list ((tensor/backward t1) (tensor/new-grad t1 (tensor/grad out-t)))
-                                                    ((tensor/backward t2) (tensor/new-grad t2 (tensor/grad out-t))))))]))))
+                    (tensor/set-presyns out-t (list ((tensor/backward t1) (tensor/set-grad t1 (tensor/grad out-t)))
+                                                    ((tensor/backward t2) (tensor/set-grad t2 (tensor/grad out-t))))))]))))
 
+;; tensor/vm-mul: Tensor Tensor -> Tensor
 (define (tensor/vm-mul t1 t2)
   (mk-tensor (ndarr/vm-mul (tensor/ndarr t1) (tensor/ndarr t2))
              (ndarr/fill (ndarr/shape (ndarr/vm-mul (tensor/ndarr t1) (tensor/ndarr t2))) 0)
@@ -292,14 +324,15 @@
                   (local
                     [(define v (first (tensor/presyns out-t)))
                      (define m (second (tensor/presyns out-t)))]
-                    (tensor/new-presyns out-t (list ((tensor/backward v) (tensor/new-grad v (ndarr/vm-mul (tensor/grad out-t) (ndarr/transpose (tensor/ndarr m)))))
-                                                    ((tensor/backward m) (tensor/new-grad m (ndarr/outer-prod (tensor/grad out-t) (tensor/ndarr v)))))))]))))
+                    (tensor/set-presyns out-t (list ((tensor/backward v) (tensor/set-grad v (ndarr/vm-mul (tensor/grad out-t) (ndarr/transpose (tensor/ndarr m)))))
+                                                    ((tensor/backward m) (tensor/set-grad m (ndarr/outer-prod (tensor/grad out-t) (tensor/ndarr v)))))))]))))
 
+;; tensor/mse: Tensor Tensor -> Tensor
 (define (tensor/mse y-pred y-targ)
   (mk-tensor (ndarr/mse (tensor/ndarr y-pred) (tensor/ndarr y-targ))
              (ndarr/fill (ndarr/shape (tensor/ndarr y-targ)) 0)
              'mse
-             (list (tensor/flag y-pred 'Y-pred) y-targ)
+             (list (tensor/set-tag y-pred 'Y-pred) y-targ)
              (lambda (self)
                (local [(define t1 ((tensor/forward (first (tensor/presyns self))) (first (tensor/presyns self))))
                        (define t2 ((tensor/forward (second (tensor/presyns self))) (second (tensor/presyns self))))]
@@ -318,18 +351,13 @@
                     [(define t1 (first (tensor/presyns out-t)))
                      (define t2 (second (tensor/presyns out-t)))
                      (define n (length t1))]
-                    (tensor/new-presyns out-t (list ((tensor/backward t1) (tensor/new-grad t1
+                    (tensor/set-presyns out-t (list ((tensor/backward t1) (tensor/set-grad t1
                                                                                           (ndarr/map (lambda (x) (* x (/ 2 n)))
                                                                                                       (ndarr/elementwise - (tensor/ndarr t1) (tensor/ndarr t2)))))
-                                                    ((tensor/backward t2) (tensor/new-grad t2
+                                                    ((tensor/backward t2) (tensor/set-grad t2
                                                                                           0)))))]))))
 
-
-;((tensor/backward t1) (tensor/new-grad t1 (ndarr/elementwise + (tensor/grad t1) (tensor/grad out-t))))
-;((tensor/backward t2) (tensor/new-grad t2 (ndarr/elementwise + (tensor/grad t2) (tensor/grad out-t)))))))]))))
-
-;((tensor/backward t1) (tensor/new-grad t1 (tensor/grad out-t)))
-;((tensor/backward t2) (tensor/new-grad t2 (tensor/grad out-t))))))]))))
+;; tensor/tanh: Tensor -> Tensor
 (define (tensor/tanh t)
   (mk-tensor (ndarr/map tanh (tensor/ndarr t)) 
              (ndarr/fill (ndarr/shape (tensor/ndarr t)) 0)
@@ -352,32 +380,15 @@
                  [else
                   (local
                     [(define t (first (tensor/presyns out-t)))]
-                    (tensor/new-presyns out-t (list ((tensor/backward t)
-                                                     (tensor/new-grad t (ndarr/elementwise * (ndarr/map tanh-deriv (tensor/ndarr t)) (tensor/grad out-t)))))))]))))
-                    ;(tensor/new-presyns out-t (list ((tensor/backward t) (tensor/new-grad t (ndarr/map tanh-deriv (tensor/grad out-t)))))))]))))
-;
-;
-;(define (tensor/mul t1 t2)
-;  (mk-tensor (ndarr/elementwise + (tensor/ndarr t1) (tensor/ndarr t2))
-;             (ndarr/fill (ndarr/shape (tensor/ndarr t1)) 0)
-;             'add
-;             (list t1 t2)
-;             ;; Tensor -> (listof Tensor)
-;             (lambda (out-t)
-;               (cond
-;                 [(tensor/leaf? out-t) out-t]
-;                 [else
-;                  (local
-;                    [(define t1 (first (tensor/presyns out-t)))
-;                     (define t2 (second (tensor/presyns out-t)))]
-;                    (tensor/new-presyns out-t (list ((tensor/backward t1) (tensor/new-grad t1 (ndarr/elementwise * (tensor/grad t2) (tensor/grad out-t))))
-;                                                    ((tensor/backward t2) (tensor/new-grad t2 (ndarr/elementwise * (tensor/grad t1) (tensor/grad out-t)))))))]))))
+                    (tensor/set-presyns out-t (list ((tensor/backward t)
+                                                     (tensor/set-grad t (ndarr/elementwise * (ndarr/map tanh-deriv (tensor/ndarr t)) (tensor/grad out-t)))))))]))))
+
 
 
 ;; Optimization functions
 ;; ~~~~~~~~~~
 
-
+;; optim/step: Tensor Num -> Tensor
 (define (optim/step t step-size)
   (mk-tensor (cond
                [(tensor/param? t) (ndarr/elementwise +
@@ -385,24 +396,27 @@
                                 (ndarr/elementwise * (ndarr/fill (ndarr/shape (tensor/ndarr t)) (- step-size)) (tensor/grad t)))]
                [else (tensor/ndarr t)])
              (tensor/grad t)
-             (tensor/prev-op t)
+             (tensor/tag t)
              (map (lambda (x) (optim/step x step-size)) (tensor/presyns t))
              (tensor/forward t)
              (tensor/backward t)))
 
+;; optim/backward: Tensor -> Tensor
 (define (optim/backward t)
-  ((tensor/backward t) (tensor/new-grad t (ndarr/fill (ndarr/shape (tensor/ndarr t)) 1))))
+  ((tensor/backward t) (tensor/set-grad t (ndarr/fill (ndarr/shape (tensor/ndarr t)) 1))))
 
+;; optim/replace-value: Tensor (Tensor -> Bool) NdArr -> Tensor
 (define (optim/replace-value t pred? value)
        (cond
-         [(pred? t) (tensor/new-ndarr t value)]
-         [else (tensor/new-presyns t (map (lambda (x) (optim/replace-value x pred? value)) (tensor/presyns t)))]))
+         [(pred? t) (tensor/set-ndarr t value)]
+         [else (tensor/set-presyns t (map (lambda (x) (optim/replace-value x pred? value)) (tensor/presyns t)))]))
 
 ;; optim/forward: Tensor NdArr -> Tensor
 (define (optim/forward t input target)
   ((tensor/forward t) (optim/replace-value (optim/replace-value t tensor/input? input) tensor/targ? target)))
 
-
+;; Runs one epoch of training
+;; optim/epoch: Tensor Num Dataset empty -> (list Tensor (listof Num))
 (define (optim/epoch t step-size dataset loss-acc)
   (cond
     [(empty? dataset) (list t loss-acc)]
@@ -414,6 +428,9 @@
        
      (optim/epoch updated step-size (rest dataset) (cons (tensor/ndarr updated) loss-acc)))]))
 
+;; Evaluates the model + loss function of the Tensor on the given dataset
+;;   and produces the mean loss
+;; optim/eval: Tensor Dataset -> Num
 (define (optim/eval t test-set)
   (local
     [(define (loop test-set loss-acc)
@@ -426,8 +443,12 @@
                        (cons (tensor/ndarr (optim/forward t X Y)) loss-acc)))]))]
   (loop test-set empty)))
 
-(define log-every-n-epochs 10)
+(define log-every-n-epochs 25)
 
+;; Runs multiple epochs of training and evaluation, producing a list of:
+;; - The resulting model
+;; - A list of mean train losses for every nth epoch
+;; - A list of mean validation losses for every nth epoch
 ;; optim/epochs: Nat Tensor Num (listof (list NdArr NdArr)) (listof (list NdArr NdArr)) empty -> (list Tensor (listof Num))
 (define (optim/epochs n t step-size train-set test-set train-loss-acc test-loss-acc)
   (cond
@@ -448,24 +469,11 @@
 ;; Data synthesis functions
 ;; ~~~~~~~~~~
 
-(define (data/synthesize-batch n)
-  (local
-    [(define (loop n acc-xx acc-xy acc-y)
-       (cond
-         [(zero? n) (list acc-xx acc-xy acc-y)]
-         [else (local
-                 [(define X_x (random/uniform -1 1))
-                  (define X_y (random/uniform -1 1))
-                  (define Y (cond
-                              ;; VERY non-linear
-                              [(and (not (zero? X_x)) (< (/ X_y X_x) (tan (sqrt (+ (sqr (* pi X_y)) (sqr (* pi X_x))))))) 1]
-                              [else -1]))]
-                 (loop (sub1 n)
-                       (cons (random/add-noise X_x) acc-xx)
-                       (cons (random/add-noise X_y) acc-xy)
-                       (cons Y acc-y)))]))]
-    (loop n empty empty empty)))
+;; A DataPair is a (list NdArr NdArr)
 
+;; A DataSet is a (listof DataBatch)
+
+;; data/synthesize-item: _ -> DataPair
 (define (data/synthesize-item _)
   (local
     [(define X_x (random/uniform -1 1))
@@ -474,36 +482,29 @@
                  ;; VERY non-linear
                  [(and (not (zero? X_x)) (< (/ X_y X_x) (tan (sqrt (+ (sqr (* pi X_y)) (sqr (* pi X_x))))))) 1]
                  [else -1]))]
-    (list (list X_x X_y) (list Y))))
+    (list (list (random/add-noise X_x) (random/add-noise X_y)) (list Y))))
 
-
+;; data/synthesize: Nat -> DataSet
 (define (data/synthesize n)
   (build-list n (lambda (_) (data/synthesize-item 0))))
-(define train-set (data/synthesize 160))
-(define test-set (data/synthesize 40))
-;(tensor/vm-mul (tensor/new-input '(0 0)) (tensor/new-param '((0.1 0.2))))
-;(tensor/mse (tensor/vm-mul (tensor/new-input '(0 0)) (tensor/new-param '((0.1 0.2))))
-;            (tensor/new-targ '(0)))
-
-;(define model (tensor/mse (tensor/tanh (tensor/vm-mul (tensor/tanh (tensor/vm-mul (tensor/tanh (tensor/vm-mul (tensor/new-input '(0.1 0.1))
-;                                                                                                              (tensor/new-param '((0.1 0.2) (0.1 0) (0 0) (0 0)))))
-;                                                                                  (tensor/new-param '((0 0 0 0) (0 0 0.1 0) (0 0.2 0 0)))))
-;                                                      (tensor/new-param '((0 0 0)))))
-;                          (tensor/new-targ '(0))))
 
 
 ;; nn/linear: Tensor Nat -> Tensor
 (define (nn/linear inp-t out-size)
   (tensor/add (tensor/vm-mul inp-t
-                             (tensor/new-param (ndarr/fill (list out-size
-                                                                 (first (ndarr/shape (tensor/ndarr inp-t))))
-                                                           0)))
-              (tensor/new-param (ndarr/fill (list out-size) 0))))
+                             (tensor/new-param (list out-size
+                                                     (first (ndarr/shape (tensor/ndarr inp-t))))))
+              (tensor/new-param (list out-size))))
 
+;; nn/mlp: (listof Nat) (Tensor -> Tensor) Tensor -> Tensor
 (define (nn/mlp sizes act_fn input)
   (cond
     [(empty? sizes) input]
     [else (nn/mlp (rest sizes) act_fn (act_fn (nn/linear input (first sizes))))]))
+
+
+(define train-set (data/synthesize 160))
+(define test-set (data/synthesize 40))
 
 (define model (nn/mlp '(4 3 1) tensor/tanh (tensor/new-input '(0 0))))
 (define loss (tensor/mse model (tensor/new-targ '(0))))
@@ -516,12 +517,12 @@
 "New model + loss function"
 '-----------------
 (first results)
-'
+
 '==============================================================
 "Train loss (in reverse order)"
 '-----------------
 (second results)
-'
+
 '==============================================================
 "Validation loss (in reverse order)"
 '-----------------
